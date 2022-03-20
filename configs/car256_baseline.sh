9f6f462c86be5b5c84b825e3ef4898bf5232a152
LeoXing <leoxing1996@gmail.com>
2022-03-20 21:46:46 +0800

    fix config data path

--- configs/car256_baseline.sh
+++ configs/car256_baseline.sh
@@ -1,2 +1,3 @@
+DATA="s3://data/compCar256/"
 SPEC=paper256
 MODEL=stylenerf_cars
--- configs/car256_baseline_wo_prog.sh
+++ configs/car256_baseline_wo_prog.sh
@@ -1,3 +1,4 @@
+DATA="s3://data/compCar256/"
 SPEC=paper256
 MODEL=stylenerf_cars
 PYTHON_ARGS="model.G_kwargs.synthesis_kwargs.progressive=False"
