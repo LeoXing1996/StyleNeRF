53b45786f809a73d5f0a597f114d6f5780c0ba06
LeoXing <leoxing1996@gmail.com>
2022-03-29 17:47:32 +0800

    revise name of config

--- configs/ffhq256_sr.sh
+++ configs/ffhq256_sr.sh
@@ -2,5 +2,5 @@ JOB_NAME=stylenerf-ffhq256-sr
 
 DATA="openmmlab:s3://openmmlab/datasets/editing/ffhq/ffhq_imgs/ffhq_256/"
 SPEC=paper256
-MODEL=stylenerf_ffhq_sr
+MODEL=stylenerf_ffhq_fullSR_scale2
 DESC=FULLSR-scale2
