74e9c20da6daa30832f3019e69c8fe77d5a13abb
LeoXing <leoxing1996@gmail.com>
2022-03-29 17:44:41 +0800

    finish full-SR config

--- configs/ffhq256_sr.sh
+++ configs/ffhq256_sr.sh
@@ -2,4 +2,5 @@ JOB_NAME=stylenerf-ffhq256-sr
 
 DATA="openmmlab:s3://openmmlab/datasets/editing/ffhq/ffhq_imgs/ffhq_256/"
 SPEC=paper256
-MODEL=stylenerf_ffhq_sr_debug
+MODEL=stylenerf_ffhq_sr
+DESC=FULLSR-scale2
--- training/training_loop.py
+++ training/training_loop.py
@@ -547,8 +547,7 @@ def training_loop(
         done = (cur_nimg >= total_kimg * 1000)
         if (not done) and (cur_tick != 0) and (
                 cur_nimg < tick_start_nimg + kimg_per_tick * 1000):
-            pass
-            # continue
+            continue
 
         # Print status line, accumulating the same information in stats_collector.  # noqa
         tick_end_time = time.time()
