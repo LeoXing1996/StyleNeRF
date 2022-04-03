7007562a582e79a3efcd64ea2e08d32813ce865e
LeoXing <leoxing1996@gmail.com>
2022-04-03 22:33:34 +0800

    avoid pass config by python args

--- configs/ffhq256_sr_resume10080_start10k.sh
+++ configs/ffhq256_sr_resume10080_start10k.sh
@@ -2,9 +2,6 @@ JOB_NAME=stylenerf-ffhq256-sr
 
 DATA="openmmlab:s3://openmmlab/datasets/editing/ffhq/ffhq_imgs/ffhq_256/"
 SPEC=paper256
-MODEL=stylenerf_ffhq_fullSR_scale2
+MODEL=stylenerf_ffhq_fullSR_scale2_start10k
 DESC=resume-10080-FULLSR-scale2-start10k
-PYTHON_ARGS=(
-    'resume=modelzoo/StyleNeRF-FFHQ-256-Baseline-010080.pkl'
-    'model.loss_kwargs.sr_curriculum=[10000,20000]'
-)
+PYTHON_ARGS='resume=modelzoo/StyleNeRF-FFHQ-256-Baseline-010080.pkl'
