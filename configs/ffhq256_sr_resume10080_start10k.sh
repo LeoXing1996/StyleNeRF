0267d0faf757cdd9bd17bf05b5eaf955b839d2d1
LeoXing <leoxing1996@gmail.com>
2022-04-04 22:23:49 +0800

    update desc

--- configs/ffhq256_sr_resume10080_start10k.sh
+++ configs/ffhq256_sr_resume10080_start10k.sh
@@ -3,5 +3,5 @@ JOB_NAME=stylenerf-ffhq256-sr
 DATA="openmmlab:s3://openmmlab/datasets/editing/ffhq/ffhq_imgs/ffhq_256/"
 SPEC=paper256
 MODEL=stylenerf_ffhq_fullSR_scale2_start10k
-DESC=resume-10080-FULLSR-scale2-start10k
+DESC=resume-10080-FULLSR-scale2-start10k-w1e-1
 PYTHON_ARGS='resume=modelzoo/StyleNeRF-FFHQ-256-Baseline-010080.pkl'
